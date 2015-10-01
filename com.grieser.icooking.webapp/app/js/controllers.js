598b321286fa026f7e7022f59c4305427c8610d3
Steffen Grieser <steffen.grieser@gmx.de>
2015-10-01 23:05:14 +0200

    WebApp returns dummy list of recipes

--- com.grieser.icooking.webapp/app/js/controllers.js
+++ com.grieser.icooking.webapp/app/js/controllers.js
@@ -1,3 +1,14 @@
 'use strict';
 
 /* Controllers */
+
+var phonecatApp = angular.module('iCookingApp', []);
+
+phonecatApp.controller('CookingRecipesController', function($scope) {
+  $scope.recipes = [
+    {'name': 'Käsespätzle',
+     'snippet': 'Natürlich schwäbisch gemacht.'},
+    {'name': 'Pizza',
+     'snippet': 'Nur mit Salami und Wurst.'}
+  ];
+});
